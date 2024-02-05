3dcb9660be8049c62cc6ca32488483a1c30faeca
Adams Lech <adams.a.lech@hotmail.com>
2024-02-05 18:59:08 -0300

    inicioa exercicios 12

---
+++ 12/index.js
@@ -0,0 +1,13 @@
+//# Exercício 12
+
+//## Taxa de Juros
+
+//Faça um programa que calcula a taxa de juros que foi operada sobre um financiamento a partir do
+// montate total que foi pago, do capital inicial que foi obtido como empréstimo e do tempo
+// decorrido do financiamento.
+
+//i = \left( \frac{M}{C} \right)^\frac{1}{n} - 1 
+
+const montantePago = 90000;
+const capitalEmprestimo = 60000;
+const tempoEmprestimo = 24;
